d0b2a538c615c11bbf4b9f097c28975dc6591d00
Tmk_Git <ST10484278@rcconnect.edu.za>
2026-05-28 20:38:07 +0200

    Implementing the GUI structure with relevant code.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,48 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace CyberNova
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private ChatBot bot = new ChatBot();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // ASCII ART HEADER
+            AsciiArtBox.Text =
+@"   _____      _               _   _                 
+  / ____|    | |             | \ | |                
+ | |    _   _| |__   ___ _ __|  \| | _____   ____ _ 
+ | |   | | | | '_ \ / _ \ '__| . ` |/ _ \ \ / / _` |
+ | |___| |_| | |_) |  __/ |  | |\  | (_) \ V / (_| |
+  \_____\__, |_.__/ \___|_|  |_| \_|\___/ \_/ \__,_|
+         __/ |                                      
+        |___/                                                                                           ";
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            string userInput = InputBox.Text;
+
+            if (string.IsNullOrWhiteSpace(userInput))
+                return;
+
+            string userName = "User";
+
+            // Show user message
+            ChatHistory.AppendText("You: " + userInput + "\n");
+
+            // Get bot response
+            string response = bot.GetResponse(userInput, userName);
+
+            // Show bot message
+            ChatHistory.AppendText("CyberNova: " + response + "\n\n");
+
+            InputBox.Clear();
+
+            ChatHistory.ScrollToEnd();
         }
     }
 }
